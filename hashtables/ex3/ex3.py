b8de8eb03a2364509d858c6f96201f45e4b547cc
rabah babaci <4rabah@gmail.com>
2020-09-12 10:51:42 -0700

    clean the code for ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,15 +1,15 @@
 def intersection(arrays):
-    result = []
-    for i in range(len(arrays)):
+    result, num_lists = [], len(arrays)
+    for i in range(num_lists):
         arrays[i] = set(arrays[i])
 
     for x in arrays[0]:
         k = 1
-        while k < len(arrays):
+        while k < num_lists:
             if x not in arrays[k]:
                 break
             k += 1
-        if k == len(arrays):
+        if k == num_lists:
             result.append(x)
     return result
 
@@ -21,4 +21,4 @@ if __name__ == "__main__":
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    # print(intersection(arrays))
+    print(intersection(arrays))

885106bbdf792f827ffac188e87671d7eb64fe30
rabah babaci <4rabah@gmail.com>
2020-09-12 10:49:54 -0700

    implement the intersection of multiple lists function

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,9 +1,16 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    for i in range(len(arrays)):
+        arrays[i] = set(arrays[i])
 
+    for x in arrays[0]:
+        k = 1
+        while k < len(arrays):
+            if x not in arrays[k]:
+                break
+            k += 1
+        if k == len(arrays):
+            result.append(x)
     return result
 
 
@@ -14,4 +21,4 @@ if __name__ == "__main__":
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    print(intersection(arrays))
+    # print(intersection(arrays))
